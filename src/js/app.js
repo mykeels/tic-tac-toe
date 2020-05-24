a879ef16f1bc7ea56251f6111c6acb95bc5c66a7
Idiakose O. Sunday <idiakosesunday@gmail.com>
2020-05-24 09:06:47 +0100

    Scale Height with wodth by sizing row container appropriately

--- src/js/app.js
+++ src/js/app.js
@@ -10,11 +10,18 @@ app.controller("GameCtrl", function ($scope, $http) {
     $scope.game_type = null;
     $scope.your_turn = true;
     $scope.network_btn = "Network Game";
-    $scope.getTileHeight = (length) => Math.ceil(Math.ceil(screen.width * (length < 6 ? 0.35 : (length < 9 ? 0.32 : 0.3))) / length);
-    $scope.getTileWidth = (length) => {
+    // $scope.getTileHeight = (length) => Math.ceil(Math.ceil(screen.width * (length < 6 ? 0.35 : (length < 9 ? 0.32 : 0.3))) / length);
+    // $scope.getTileHeight = (length) => `calc(25vw)`
+    $scope.getTileHeight = (length) => {
         return length < 7 ? 'calc(' + Math.floor(75 / length) + '% - 4px)' :
+        '10%'
+    }
+    $scope.getTileWidth = (length) => {
+        return length < 7 ? 'calc(' + Math.floor(100 / length) + '% - 4px)' :
             '10%'
     };
+    $scope.getRowContainerHeight = (length) => `calc(${Math.floor(100 / length)}vw - 6px)`
+    $scope.getTileLineHeight = (length) => Math.ceil(Math.ceil(screen.width * (length < 6 ? 0.35 : (length < 9 ? 0.32 : 0.3))) / length);
     $scope.getFontSize = (length) => $scope.tileFontSizes[length] || 60;
     $scope.tileFontSizes = { 3: 50, 4: 42, 5: 32, 6: 22, 7: 21, 8: 18, 9: 15, 10: 12 }
     $scope.changeTiles = function () {
